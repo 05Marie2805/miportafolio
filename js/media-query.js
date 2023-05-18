c187e226810e18cd3da1ee434dd865b3a802669a
05Marie2805 <mariafernanda31234@gmail.com>
2023-05-18 10:55:43 -0500

    cambios12

--- js/media-query.js
+++ js/media-query.js
@@ -1,11 +1,10 @@
-let menuBar= document.getElementById("menuBar");
-let mainMenu= document. getElementById ("mainMenu");
-
+let menuBar = document.getElementById("menuBar");
+let mainMenu = document. getElementById("mainMenu");
 
 menuBar.addEventListener("click", function() {
-    if (mainMenu.classList.contains("main-menu")) { 
+    if (mainMenu.classList.contains("main-menu")) {
         mainMenu.classList.add("main-menu-block");
-        mainMenu.classList.remove("main-menu"); 
+        mainMenu.classList.remove("main-menu");
     }
     else {
         mainMenu.classList.add("main-menu");
